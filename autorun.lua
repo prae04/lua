127c9bcf9175496e46fb20edb30ec71c4aa9c900
Root <root@example.com>
2025-02-09 13:03:12 -0500

    use filename instead of pattern

--- autorun.lua
+++ autorun.lua
@@ -6,13 +6,14 @@ local attach_to_buffer = function(output_bufnr, pattern, command)
 		group = vim.api.nvim_create_augroup("AutoBashExec", { clear = true }),
 		--pattern = "test.sh",
 		pattern = pattern,
+		--pattern = vim.api.nvim_buf_get_name(0),
 		callback = function()
 			local append_data = function(_, data)
 				if data then
 					vim.api.nvim_buf_set_lines(output_bufnr, -1, -1, false, data)
 				end
 			end
-			vim.api.nvim_buf_set_lines(output_bufnr, 0, -1, false, { "Output of test.sh:" })
+			vim.api.nvim_buf_set_lines(output_bufnr, 0, -1, false, { string.format("Output of %s:", pattern) })
 			--vim.fn.jobstart("bash -c 'source test.sh'", {
 			vim.fn.jobstart(command, {
 				stdout_buffered = true,
@@ -26,10 +27,10 @@ end
 vim.api.nvim_create_user_command("AutoRun", function()
 	print("AutoRun starts now...")
 	local bufnr = vim.fn.input("Bufnr: ")
-	local pattern = vim.fn.input("Pattern: ")
+	--local pattern = vim.fn.input("Pattern: ")
+	local pattern = vim.api.nvim_buf_get_name(0)
 	--local command = vim.split(vim.fn.input("Command: "), ":")
 	local command = string.format('bash -c "source %s"', pattern)
 	--local command = "bash -c 'source test.sh'"
-	print(command)
 	attach_to_buffer(tonumber(bufnr), pattern, command)
 end, {})
